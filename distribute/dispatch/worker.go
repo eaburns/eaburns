997af9a1ee87b547e5ebef12fbcb63397c3dd237
Ethan Burns <burns.ethan@gmail.com>
2012-02-29 19:26:19 -0500

    distribute: fix dispatcher's RPC accept loop.

--- distribute/dispatch/worker.go
+++ distribute/dispatch/worker.go
@@ -11,21 +11,16 @@ import (
 var port = flag.Int("p", 1235, "The port on which to listen for new workers")
 
 func startWorkers(joblist *joblist) {
-	go startListening(joblist)
-	for _, addr := range flag.Args() {
-		go worker(addr, joblist)
-	}
-}
-
-func startListening(joblist *joblist) {
 	rpc.Register(&WorkerList{joblist})
 	l, err := net.Listen("tcp", ":" + strconv.Itoa(*port))
 	if err != nil {
 		logfile.Fatal("listen error:", err)
 	}
 	logfile.Print("listening for new workers on ", l.Addr())
-	for {
-		rpc.Accept(l)
+	go rpc.Accept(l);
+
+	for _, addr := range flag.Args() {
+		go worker(addr, joblist)
 	}
 }
 
