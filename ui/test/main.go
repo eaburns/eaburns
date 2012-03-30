e169c02f0695732a3ff9be2d893a72a84c5ff7b6
Ethan Burns <burns.ethan@gmail.com>
2012-03-30 15:24:32 -0400

    ui: some small cleanups.

--- ui/test/main.go
+++ ui/test/main.go
@@ -3,33 +3,23 @@ package main
 import (
 	"code.google.com/p/eaburns/ui"
 	"fmt"
-	"os"
 )
 
-func init() {
-	ui.Init()
-}
-
 func main() {
+	ui.Init()
 	defer ui.Deinit()
 
 	if err := ui.OpenWindow(200, 200); err != nil {
 		panic(err)
 	}
 
-	f, err := os.Open("gopher.png")
+	img, err := ui.LoadPng("gopher.png")
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
-
-	img, err := ui.ReadImage(f)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Loaded\n")
-	img.W = 64
-	img.H = 64
+	fmt.Printf("Loaded the image\n")
+	// Resize the image
+	img.Width, img.Height = 64, 64
 
 	ui.Clear()
 	img.Draw(0, 0)
