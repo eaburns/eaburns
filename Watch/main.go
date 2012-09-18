b6a7172bfa50f832904b87baea19ae7a230c72bb
Ethan Burns <burns.ethan@gmail.com>
2012-09-18 15:15:35 -0400

    Watch: new non-polling implementation.
    Use github.com/howeyc/fsnotify, a portable
    interface for directory change notifications.

--- Watch/main.go
+++ Watch/main.go
@@ -6,84 +6,142 @@
 package main
 
 import (
+	"code.google.com/p/goplan9/plan9/acme"
+	"github.com/howeyc/fsnotify"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
-	"os"
 	"time"
-	"flag"
-	"code.google.com/p/goplan9/plan9/acme"
 )
 
-func main() {
-	flag.Parse()
-	args := flag.Args()
+type runRequest struct {
+	time time.Time
+	done chan<- bool
+}
 
+func main() {
 	win, err := acme.New()
 	if err != nil {
-		log.Fatal("acme error: ", err)
+		log.Fatal(err)
 	}
-
-	pwd, _ := os.Getwd()
-	win.Name(pwd + "/+watch")
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	win.Name(wd + "/+watch")
 	win.Ctl("clean")
 	win.Fprintf("tag", "Get ")
 
-	run := make(chan chan<- bool)
-	go checker(run)
+	run := make(chan runRequest)
 	go events(win, run)
+	go runner(win, run)
+	watcher(wd, run)
+}
 
+// Everything but the FSN_CREATE flag since create
+// seems to imply a modify.	
+const watchFlags = fsnotify.FSN_MODIFY | fsnotify.FSN_DELETE | fsnotify.FSN_RENAME
+
+func watcher(wd string, run chan<- runRequest) {
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := w.WatchFlags(wd, watchFlags); err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
 	for {
-		done := <-run
-		cmd := exec.Command(args[0], args[1:]...)
-		r, w, err := os.Pipe()
-		if err != nil {
+		select {
+		case ev := <-w.Event:
+			switch info, err := os.Stat(ev.Name); {
+			case os.IsNotExist(err):
+				run <- runRequest{time.Now(), done}
+			case err != nil:
+				log.Println(err)
+				continue
+			default:
+				run <- runRequest{info.ModTime(), done}
+			}
+			<-done
+		case err := <-w.Error:
 			log.Fatal(err)
 		}
-		win.Addr(",")
-		win.Write("data", nil)
-		win.Ctl("clean")
-		win.Fprintf("body", "$ %s\n", strings.Join(args, " "))
-
-		cmd.Stdout = w
-		cmd.Stderr = w
-		if err := cmd.Start(); err != nil {
-			r.Close()
-			w.Close()
-			win.Fprintf("body", "%s: %s\n", strings.Join(args, " "), err)
-			done <- true
-			continue
-		}
-
-		w.Close()
-		buf := make([]byte, 4096)
-		for {
-			n, err := r.Read(buf)
-			if err != nil {
-				break
-			}
-			win.Write("body", buf[:n])
-		}
-		if err := cmd.Wait(); err != nil {
-			win.Fprintf("body", "%s: %s\n", strings.Join(args, " "), err)
-		}
-		win.Fprintf("body", "$\n")
-		win.Fprintf("addr", "#0")
-		win.Ctl("dot=addr")
-		win.Ctl("show")
-		win.Ctl("clean")
-		done <- true
 	}
 }
 
-func events(win *acme.Win, run chan chan<- bool) {
+func runner(win *acme.Win, reqs <-chan runRequest) {
+	runCommand(win)
+	last := time.Now()
+
+	for req := range reqs {
+		if last.Before(req.time) {
+			runCommand(win)
+		}
+		last = time.Now()
+		req.done <- true
+	}
+}
+
+type BodyWriter struct {
+	*acme.Win
+}
+
+func (b BodyWriter) Write(data []byte) (int, error) {
+	return b.Win.Write("body", data)
+}
+
+func runCommand(win *acme.Win) {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("Must supply a command")
+	}
+	cmdStr := strings.Join(args, " ")
+
+	win.Addr(",")
+	win.Write("data", nil)
+	win.Ctl("clean")
+	win.Fprintf("body", "$ %s\n", cmdStr)
+
+	cmd := exec.Command(args[0], args[1:]...)
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+	cmd.Stdout = w
+	cmd.Stderr = w
+
+	if err := cmd.Start(); err != nil {
+		win.Fprintf("body", "%s: %s\n", cmdStr, err)
+		return
+	}
+	w.Close()
+
+	go io.Copy(BodyWriter{win}, r)
+
+	if err := cmd.Wait(); err != nil {
+		win.Fprintf("body", "%s: %s\n", cmdStr, err)
+	}
+
+	win.Fprintf("body", "%s\n", time.Now())
+	win.Fprintf("addr", "#0")
+	win.Ctl("dot=addr")
+	win.Ctl("show")
+	win.Ctl("clean")
+}
+
+func events(win *acme.Win, run chan<- runRequest) {
 	done := make(chan bool)
 	for e := range win.EventChan() {
 		switch e.C2 {
 		case 'x', 'X': // execute
 			if string(e.Text) == "Get" {
-				run <- done
-				<- done
+				run <- runRequest{time.Now(), done}
+				<-done
 				continue
 			}
 			if string(e.Text) == "Del" {
@@ -93,66 +151,4 @@ func events(win *acme.Win, run chan chan<- bool) {
 		win.WriteEvent(e)
 	}
 	os.Exit(0)
-}
-
-func checker(run chan chan<- bool) {
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	last := time.Now()
-	done := make(chan bool)
-	known := make(map[string]bool)
-
-	for {
-		changed := false
-		cur := make(map[string]bool)
-
-		for _, n := range ents(pwd) {
-			info, err := os.Stat(n)
-			if err != nil && os.IsNotExist(err) {
-				changed = true
-				continue			
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			if !known[n] || last.Before(info.ModTime()) {
-				changed = true
-			}
-			known[n] = true
-			cur[n] = true
-		}
-	
-		for n := range known {
-			if !cur[n] {
-				changed = true
-				delete(known, n)
-			}
-		}
-
-		if changed {
-			run <- done
-			<- done
-			last = time.Now()
-		}
-
-		<- time.After(1)
-	}
-}
-
-func ents(path string) []string {
-	dir, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dir.Close()
-
-	names, err := dir.Readdirnames(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return names
 }
