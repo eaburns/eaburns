3b1c51c0b47431f59829c11fe077d6a022a09ceb
Ethan Burns <burns.ethan@gmail.com>
2012-09-25 16:22:48 -0400

    showplot: draw two plots to the same window.

--- showplot/main.go
+++ showplot/main.go
@@ -10,6 +10,8 @@ import (
 	"code.google.com/p/x-go-binding/ui/x11"
 	"image/color"
 	"image/draw"
+	"math"
+	"math/rand"
 )
 
 func main() {
@@ -30,43 +32,115 @@ func main() {
 func drawPlot(img draw.Image) {
 	c, w, h := vgimg.NewImage(img)
 	da := plot.NewDrawArea(c, w, h)
-	p := makePlot()
-	p.Draw(da)
+
+	left := *da
+	left.Size.X /= 2
+	hist := histPlot()
+	hist.Draw(left)
+
+	right := *da
+	right.Min.X = left.Min.X + left.Size.X
+	right.Size.X /= 2
+	lines := linesPlot()
+	lines.Draw(right)
 }
 
-// makePlot returns one of the example plots.
-// In this particluar case, it's the example
-// bar chart.
-func makePlot() *plot.Plot {
-	groupA := plotter.Values{20, 35, 30, 35, 27}
-	groupB := plotter.Values{25, 32, 34, 20, 25}
-	groupC := plotter.Values{12, 28, 15, 21, 8}
+func histPlot() *plot.Plot {
+	// Draw some random values from the standard
+	// normal distribution.
+	rand.Seed(int64(0))
+	v := make(plotter.Values, 10000)
+	for i := range v {
+		v[i] = rand.NormFloat64()
+	}
 
+	// Make a plot and set its title.
 	p, err := plot.New()
 	if err != nil {
 		panic(err)
 	}
-	p.Title.Text = "Bar chart"
-	p.Y.Label.Text = "Heights"
+	p.Title.Text = "Histogram"
 
-	w := vg.Points(20)
+	// Create a histogram of our values drawn
+	// from the standard normal.
+	h := plotter.NewHist(v, 16)
+	// Normalize the area under the histogram to
+	// sum to one.
+	h.Normalize(1)
+	p.Add(h)
 
-	barsA := plotter.NewBarChart(groupA, w)
-	barsA.Color = color.RGBA{R: 255, G: 67, B: 67, A: 255}
-	barsA.Offset = -w
-
-	barsB := plotter.NewBarChart(groupB, w)
-	barsB.Color = color.RGBA{R: 67, G: 255, B: 67, A: 255}
-
-	barsC := plotter.NewBarChart(groupC, w)
-	barsC.Color = color.RGBA{R: 67, G: 67, B: 255, A: 255}
-	barsC.Offset = w
-
-	p.Add(barsA, barsB, barsC)
-	p.Legend.Add("Group A", barsA)
-	p.Legend.Add("Group B", barsB)
-	p.Legend.Add("Group C", barsC)
-	p.Legend.Top = true
-	p.NominalX("One", "Two", "Three", "Four", "Five")
+	// The normal distribution function
+	norm := plotter.NewFunction(stdNorm)
+	norm.Color = color.RGBA{R: 255, A: 255}
+	norm.Width = vg.Points(2)
+	p.Add(norm)
 	return p
+}
+
+// stdNorm returns the probability of drawing a
+// value from a standard normal distribution.
+func stdNorm(x float64) float64 {
+	const sigma = 1.0
+	const mu = 0.0
+	const root2π = 2.50662827459517818309
+	return 1.0 / (sigma * root2π) * math.Exp(-((x-mu)*(x-mu))/(2*sigma*sigma))
+}
+
+func linesPlot() *plot.Plot {
+	// Get some random points
+	rand.Seed(int64(0))
+	n := 15
+	scatterData := randomPoints(n)
+	lineData := randomPoints(n)
+	linePointsData := randomPoints(n)
+
+	// Create a new plot, set its title and
+	// axis labels.
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Points Example"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	// Draw a grid behind the data
+	p.Add(plotter.NewGrid())
+
+	// Make a scatter plotter and set its style.
+	s := plotter.NewScatter(scatterData)
+	s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
+
+	// Make a line plotter and set its style.
+	l := plotter.NewLine(lineData)
+	l.LineStyle.Width = vg.Points(1)
+	l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+	l.LineStyle.Color = color.RGBA{B: 255, A: 255}
+
+	// Make a line plotter with points and set its style.
+	lpLine, lpPoints := plotter.NewLinePoints(linePointsData)
+	lpLine.Color = color.RGBA{G: 255, A: 255}
+	lpPoints.Shape = plot.PyramidGlyph{}
+	lpPoints.Color = color.RGBA{R: 255, A: 255}
+
+	// Add the plotters to the plot, with a legend
+	// entry for each
+	p.Add(s, l, lpLine, lpPoints)
+	p.Legend.Add("scatter", s)
+	p.Legend.Add("line", l)
+	p.Legend.Add("line points", lpLine, lpPoints)
+	return p
+}
+
+// randomPoints returns some random x, y points.
+func randomPoints(n int) plotter.XYs {
+	pts := make(plotter.XYs, n)
+	for i := range pts {
+		if i == 0 {
+			pts[i].X = rand.Float64()
+		} else {
+			pts[i].X = pts[i-1].X + rand.Float64()
+		}
+		pts[i].Y = pts[i].X + 10*rand.Float64()
+	}
+	return pts
 }
