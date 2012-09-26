d70c5cdf4b0ad98ce2b6b6d5ea6d817acc32a93b
Ethan Burns <burns.ethan@gmail.com>
2012-09-26 16:39:39 -0400

    showplot: convert draw?data coordinates.

--- showplot/main.go
+++ showplot/main.go
@@ -7,49 +7,138 @@ import (
 	"code.google.com/p/plotinum/plotter"
 	"code.google.com/p/plotinum/vg"
 	"code.google.com/p/plotinum/vg/vgimg"
+	"code.google.com/p/x-go-binding/ui"
 	"code.google.com/p/x-go-binding/ui/x11"
+	"fmt"
 	"image/color"
 	"image/draw"
 	"math"
 	"math/rand"
 )
 
+type plots [2]struct {
+	plot *plot.Plot
+	dataArea plot.DrawArea
+}
+
+var ps plots
+
+var font vg.Font
+
 func main() {
+	ps[0].plot = linesPlot()
+	ps[1].plot = histPlot()
+
+	var err error
+	font, err = vg.MakeFont("Times-Roman", vg.Points(12))
+	if err != nil {
+		panic(err)
+	}
+
 	win, err := x11.NewWindow()
 	if err != nil {
 		panic(err)
 	}
 
-	drawPlot(win.Screen())
+	drawPlots(win.Screen())
 	win.FlushImage()
 
 	events := win.EventChan()
-	for _ = range events {
+	for ev := range events {
+		if m, ok := ev.(ui.MouseEvent); ok && m.Buttons == 1{
+			winHeight := 600	// hard-coded for ui/x11…
+			p, x, y := dataCoord(m.Loc.X, winHeight-m.Loc.Y)
+			if p >= 0 {
+				str := fmt.Sprintf("plot: %d, coord: %g, %g\n", p, x, y)
+				crosshair(win.Screen(), m.Loc.X, winHeight-m.Loc.Y, str)
+				win.FlushImage()
+			}
+		}
 	}
 }
 
-// drawPlot draws the plot to an image.
-func drawPlot(img draw.Image) {
+// drawPlots draws the plots to an image.
+func drawPlots(img draw.Image) {
 	c, w, h := vgimg.NewImage(img)
 	da := plot.NewDrawArea(c, w, h)
 
+	textAreaSize := vg.Points(12)
+	da.Min.Y += textAreaSize
+	da.Size.Y -= textAreaSize
+
 	left := *da
 	left.Size.X /= 2
-	hist := histPlot()
-	hist.Draw(left)
+	ps[0].plot.Draw(left)
+	ps[0].dataArea = ps[0].plot.DataDrawArea(left)
 
 	right := *da
 	right.Min.X = left.Min.X + left.Size.X
 	right.Size.X /= 2
-	lines := linesPlot()
-	lines.Draw(right)
+	ps[1].plot.Draw(right)
+	ps[1].dataArea = ps[1].plot.DataDrawArea(right)
+}
+
+// crosshair draws a plus at the given point.
+func crosshair(img draw.Image, x, y int, str string) {
+	c, _, _ := vgimg.NewImage(img)
+
+	// drawPlots here because NewImage
+	// clears the canvas.  Instead, the canvas
+	// should just be stored instead of being
+	// recreated at each redraw.
+	drawPlots(img)
+
+	c.SetColor(color.RGBA{R:255, A:255})
+
+	xc := vg.Inches(float64(x)/c.DPI())
+	yc := vg.Inches(float64(y)/c.DPI())
+	radius := vg.Points(5)
+
+	var p vg.Path
+	p.Move(xc-radius, yc)
+	p.Line(xc+radius, yc)
+	c.Stroke(p)
+
+	p = vg.Path{}
+	p.Move(xc, yc+radius)
+	p.Line(xc, yc-radius)
+	c.Stroke(p)
+
+	c.SetColor(color.Black)
+	c.FillString(font, vg.Length(0), vg.Length(0), str)
+}
+
+// dataCoord returns the plot number and data
+// coordinate of a screen coordinate.  A negative
+// plot numbers means that the screen coordinate
+// is not in the data area of any plot.
+func dataCoord(x, y int) (int, float64, float64) {
+	dpi := ps[0].dataArea.DPI()
+	pt := plot.Point{
+		X: vg.Inches(float64(x)/dpi),
+		Y: vg.Inches(float64(y)/dpi),
+	}
+	for i, p := range ps {
+		if p.dataArea.Contains(pt) {
+			da := p.dataArea
+			x := float64((pt.X - da.Min.X)/(da.Max().X-da.Min.X))
+			x *= (p.plot.X.Max-p.plot.X.Min)
+			x += p.plot.X.Min
+
+			y := float64((pt.Y - da.Min.Y)/(da.Max().Y-da.Min.Y))
+			y *= (p.plot.Y.Max-p.plot.Y.Min)
+			y += p.plot.Y.Min
+			return i, x, y
+		}
+	}
+	return -1, 0, 0
 }
 
 func histPlot() *plot.Plot {
 	// Draw some random values from the standard
 	// normal distribution.
 	rand.Seed(int64(0))
-	v := make(plotter.Values, 10000)
+	v := make(plotter.Values, 1000)
 	for i := range v {
 		v[i] = rand.NormFloat64()
 	}
@@ -89,7 +178,7 @@ func stdNorm(x float64) float64 {
 func linesPlot() *plot.Plot {
 	// Get some random points
 	rand.Seed(int64(0))
-	n := 15
+	n := 10
 	scatterData := randomPoints(n)
 	lineData := randomPoints(n)
 	linePointsData := randomPoints(n)
