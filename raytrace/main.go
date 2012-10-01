19db513ed061768e11dabacb4e88badad22f933e
Ethan Burns <burns.ethan@gmail.com>
2012-10-01 18:16:23 -0400

    raytrace: add proper projection.

--- raytrace/main.go
+++ raytrace/main.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"image/png"
 	"math"
+	"math/rand"
 	"os"
 )
 
@@ -12,42 +13,60 @@ var scene = Scene{
 	AmbientLight: Color{0.25, 0.25, 0.25},
 
 	Lights: []Light{
-		{Point{-1.0, 1.0, -0.2}, Color{1.0, 1.0, 1.0}},
-		{Point{1.0, 1.0, -0.2}, Color{0.0, 0.0, 1.0}},
+		{Point{1.0, 1.0, 0.3}, Color{1.0, 1.0, 1.0}},
+		{Point{-1.0, -1.0, 0.3}, Color{1.0, 1.0, 1.0}},
 	},
 
 	Objects: []Object{
 		Solid{
 			C:     Color{1.0, 0.0, 0.0},
 			Shine: 3.0,
-			Shape: Sphere{Point{0.25, 0.20, -0.5}, 0.25},
+			Shape: Sphere{Point{0.0, 0.0, 0.0}, 0.25},
+		},
+		Solid{
+			C:     Color{0.0, 0.0, 1.0},
+			Shine: 5.0,
+			Shape: Sphere{Point{0.1, 0.1, 1.0}, 0.125},
 		},
 		Solid{
 			C:     Color{0.0, 1.0, 0.0},
 			Shine: 3.0,
-			Shape: Sphere{Point{0.75, 0.75, -0.5}, 0.25},
+			Shape: Sphere{Point{0.5, 0.5, 0.0}, 0.25},
 		},
 	},
 }
 
-var eye = Point{0.5, 0.5, 1.0}
+var (
+	eye = Point{0, 0, 3}
+	ref = Point{0, 0, 0}
+	up = Point{0, 1, 0}
+)
+
+const NSamples = 8
 
 func main() {
 	img := image.NewRGBA(image.Rect(0, 0, 480, 480))
 	b := img.Bounds()
+
+	image2World := makeProjection(b)
+
 	for x := b.Min.X; x < b.Max.X; x++ {
 		for y := b.Min.Y; y < b.Max.Y; y++ {
-			px := img2World(b, float64(x), float64(y))
-			dir := px.Minus(eye)
-			pxDist := dir.Dot(dir)
-
-			hit, ok := scene.Hit(eye, dir)
-			if !ok {
-				img.Set(x, y, color.Black)
-			} else if hit.Distance > pxDist {
-				c := hit.Object.Color(scene, hit, 0)
-				img.Set(x, y, c.ImageColor())
+			color := Color{0, 0, 0}
+			for i := 0; i < NSamples; i++ {
+				xjitter := rand.Float64()/2 - 1/4.0
+				yjitter := rand.Float64()/2 - 1/4.0
+				px := image2World(float64(x)+xjitter, float64(y)+yjitter)
+				dir := px.Minus(eye)
+	
+				hit, ok := scene.Hit(eye, dir)
+				c := Color{0, 0, 0}
+				if ok {
+					c = hit.Object.Color(scene, hit, 0)
+				}
+				color = Color(Point(color).Plus(Point(c).Scale(1.0/NSamples)))
 			}
+			img.Set(x, y, Color(color).ImageColor())
 		}
 	}
 
@@ -68,13 +87,41 @@ func main() {
 // to a point in the 3-dimensional world.
 //
 // TODO: This is not setup correctly.
-func img2World(b image.Rectangle, x, y float64) Point {
+func image2World(b image.Rectangle, x, y float64) Point {
 	w := float64(b.Max.X - b.Min.X)
 	h := float64(b.Max.Y - b.Min.Y)
 	return Point{
 		(x - float64(b.Min.X)) / w,
 		1.0 - (y-float64(b.Min.Y))/h,
 		0.0,
+	}
+}
+
+func makeProjection(b image.Rectangle) func(x, y float64)Point {
+	z := eye.Minus(ref).Normalize()
+	x := up.Cross(z).Normalize()
+	y := z.Cross(x).Normalize()
+	m := [4][4]float64{
+		{ x[0], y[0], z[0], eye[0] },
+		{ x[1], y[1], z[1], eye[1] },
+		{ x[2], y[2], z[2], eye[2] },
+	}
+	clipL, clipR := -1.0, 1.0
+	clipB, clipT := -1.0, 1.0
+	clipN := 2.0
+	dx := (clipR-clipL)/float64(b.Max.X-b.Min.X)
+	height := float64(b.Max.Y-b.Min.Y)
+	dy := (clipT-clipB)/height
+	zv := -clipN
+
+	return func(x, y float64) Point {
+		xv := clipL + dx*x
+		yv := clipB + dy*(height - y)
+		var w Point
+		w[0] = m[0][0]*xv + m[0][1]*yv + m[0][2]*zv + m[0][3]
+		w[1] = m[1][0]*xv + m[1][1]*yv + m[1][2]*zv + m[1][3]
+		w[2] = m[2][0]*xv + m[2][1]*yv + m[2][2]*zv + m[2][3]
+		return w
 	}
 }
 
@@ -86,11 +133,20 @@ func (a Point) Dot(b Point) float64 {
 	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
 }
 
+// Cross returns the cross-product of two points.
+func (a Point) Cross(b Point) Point {
+	return Point{
+		a[1]*b[2] - a[2]*b[1],
+		a[2]*b[0] - a[0]*b[2],
+		a[0]*b[1] - a[1]*b[0],
+	}
+}
+
 // Scale returns a point with all of its
 // components scaled by a constant
 // factor.
-func (a Point) Scale(f float64) Point {
-	return Point{a[0] * f, a[1] * f, a[2] * f}
+func (p Point) Scale(f float64) Point {
+	return Point{p[0] * f, p[1] * f, p[2] * f}
 }
 
 // Minus returns a new point that is the
@@ -112,8 +168,8 @@ func (a Point) Times(b Point) Point {
 }
 
 // Normalize returns a normalized point.
-func (a Point) Normalize() Point {
-	return a.Scale(1.0 / math.Sqrt(a.Dot(a)))
+func (p Point) Normalize() Point {
+	return p.Scale(1.0 / math.Sqrt(p.Dot(p)))
 }
 
 // Color is an RGB color.
