526a1775228d4730f841432173dd61589d49a16f
Ethan Burns <burns.ethan@gmail.com>
2012-10-10 08:35:02 -0400

    9irc: fix text input, add a simple prompt.

--- irc/9irc/9irc.go
+++ irc/9irc/9irc.go
@@ -5,18 +5,26 @@ import (
 	"code.google.com/p/goplan9/plan9/acme"
 	"flag"
 	"fmt"
+	"net"
 	"log"
 	"os"
 	"strings"
 	"sort"
 	"time"
+	"bytes"
+	"unicode/utf8"
 )
+
+const defaultPort = "6667"
+
+const prompt = "\n> "
 
 var (
 	nick = flag.String("n", os.Getenv("USER"), "nick name")
 	full = flag.String("f", os.Getenv("USER"), "full name")
 	pass = flag.String("p", "", "password")
 	debug = flag.Bool("d", false, "debugging")
+	server =""
 )
 
 var (
@@ -37,32 +45,72 @@ var (
 )
 
 func main() {
+	flag.Usage = func(){
+		os.Stdout.WriteString("usage: 9irc [options] <server>[:<port>]\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 	if len(flag.Args()) != 1 {
-		os.Stdout.WriteString("usage: 9irc [-f fullname] [-n nick] <server>\n")
+		flag.Usage()
 		os.Exit(1)
+	}
+
+	var err error
+	var port string
+	if server, port, err = net.SplitHostPort(flag.Arg(0)); err != nil {
+		port = defaultPort
+		server = flag.Arg(0)
 	}
 
 	serverWin = newWindow("")
 
+/*
 	var err error
-	client, err = irc.DialServer(flag.Arg(0)+":6667", *nick, *full, *pass)
+	client, err = irc.DialServer(server+ ":" + port, *nick, *full, *pass)
 	if err != nil {
 		log.Fatal(err)
 	}
 	serverWin.Fprintf("tag", "Join ")
+*/
+	_ = port
+	client = new(irc.Client)
+	client.In = make(chan irc.Msg)
+	o := make(chan irc.Msg)
+	client.Out = o
+	client.Errors = make(chan error)
+	go func() {
+		for _ = range o {
+		}
+	}()
+	tick := time.NewTicker(time.Second*time.Duration(5))
 
 	for {
 		select {
 		case ev := <- winEvents:
+			fmt.Printf("paddr=%d, eaddr=%d\n", ev.promptAddr, ev.entryAddr)
 			handleWindowEvent(ev)
+			fmt.Printf("	-> paddr=%d, eaddr=%d\n", ev.promptAddr, ev.entryAddr)
 
-		case msg, ok := <-client.In:		
+		case msg, ok := <-client.In:	
 			if !ok {
 				serverWin.WriteString("Disconnected")
 				os.Exit(0)
 			}
 			handleMsg(msg)
+
+		case err := <-client.Errors:
+			log.Println(err)
+			for err := range client.Errors {
+				log.Println(err)
+			}
+			os.Exit(1)
+
+		case <-tick.C:
+			s := serverWin.privMsgString("tick", fmt.Sprintf("%d %d %s",
+				serverWin.promptAddr,
+				serverWin.entryAddr,
+				time.Now().Format("15:04:05")))
+			serverWin.WriteString(s)
 		}
 	}
 }
@@ -71,24 +119,35 @@ func main() {
 type window struct {
 	*acme.Win
 
-	// target is the target of this window.  It
+	// PromptAddr is the address of the empty
+	// string just before the prompt.  This is
+	// the address at which incoming messages
+	// will be displayed.
+	promptAddr int
+
+	// EntryAddr is the address of the empty
+	// byte just after the prompt after which
+	// is the user's input.
+	entryAddr int
+
+	// Target is the target of this window.  It
 	// is either a channel name, a nick name
 	// or empty (for the server window).
 	target string
 
-	// users is all users currently in this channel
+	// Users is all users currently in this channel
 	// excluding one's self.
 	users map[string]*user
 
-	// who is a list of users gathered by a who
+	// Who is a list of users gathered by a who
 	// command.
 	who []string
 
-	// lastMsgOrigin is the nick name of the last
+	// LastMsgOrigin is the nick name of the last
 	// user to send a private message.
 	lastMsgOrigin string
 
-	// lastMsgTime is the time at which the last
+	// LastMsgTime is the time at which the last
 	// private message was sent.
 	lastMsgTime time.Time
 }
@@ -117,18 +176,20 @@ func newWindow(target string) *window {
 	if err != nil {
 		log.Fatal(err)
 	}
-	name := "/irc/" + flag.Arg(0)
+	name := "/irc/" + server
 	if target != "" {
 		name += "/" + target
 	}
 	win.Name(name)
 	win.Ctl("clean")
+	win.Write("body", []byte(prompt))
 	if len(target) > 0 && target[0] == '#' {
 		win.Fprintf("tag", "Who ")
 	}
 
 	w := &window {
 		Win: win,
+		entryAddr: utf8.RuneCountInString(prompt),
 		target: target,
 		users: make(map[string]*user),
 		lastMsgTime: time.Now(),
@@ -142,22 +203,139 @@ func newWindow(target string) *window {
 	return w
 }
 
-// WriteString writes a string to the body of a window.
-func (w *window) WriteString(str string) error {
+// privMsgString returns the string that should
+// be written to this window for a message.
+func (w *window) privMsgString(who, text string) string {
+	const actionPrefix = "\x01ACTION"
+	if strings.HasPrefix(text, actionPrefix) {
+		text = strings.TrimRight(text[len(actionPrefix):], "\x01")
+		w.lastMsgOrigin = ""
+		w.lastMsgTime = time.Now()
+		return who + text
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, 512))
+
+	// Only print the user name if there is a
+	// new speaker or if two minutes has passed.
+	if who != w.lastMsgOrigin || time.Since(w.lastMsgTime).Minutes() > 2 {
+		buf.WriteRune('<')
+		buf.WriteString(who)
+		buf.WriteRune('>')
+
+		if u, ok := users[who]; ok {
+			// If the user hasn't change their nick in an hour
+			// then set this as the original nick name.
+			if time.Since(u.lastChange).Hours() > 1 {
+				u.origNick = u.nick
+			}
+			if u.nick != u.origNick {
+				buf.WriteString(" (")
+				buf.WriteString(u.origNick)
+				buf.WriteRune(')')
+			}
+		}
+		buf.WriteRune('\n')
+	}
+	w.lastMsgOrigin = who
+	w.lastMsgTime = time.Now()
+
+	if strings.HasPrefix(text, *nick + ":") {
+		buf.WriteRune('*')
+		buf.WriteString(text)
+	}
+	buf.WriteRune('\t')
+	buf.WriteString(text)
+	return buf.String()
+}
+
+// writeData writes all of the given bytes to the
+// data file.  Uses a chunk size that is small enough
+// that acme won't choke on it.
+func (w *window) writeData(data []byte) {
 	const maxWrite = 512
-	bytes := []byte(str + "\n")
-	for len(bytes) > 0 {
-		sz := len(bytes)
+	for len(data) > 0 {
+		sz := len(data)
 		if sz > maxWrite {
 			sz = maxWrite
 		}
-		n, err := w.Write("body", bytes[:sz])
+		n, err := w.Write("data", data[:sz])
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
-		bytes = bytes[n:]
+		data = data[n:]
 	}
-	return nil
+}
+
+// WriteString writes a string to the body of a window.
+func (w *window) WriteString(str string) {
+	w.Addr("#%d", w.promptAddr)
+	data := []byte(str + "\n")
+	w.writeData(data)
+
+	nr := utf8.RuneCount(data)
+	w.promptAddr += nr
+	w.entryAddr += nr
+}
+
+func (w *window) typing(q0, q1 int) {
+	const maxWrite = 512
+
+	if q0 < w.promptAddr {
+		w.promptAddr += q1 - q0
+		// w.textAddr >= w.promptAddr so this
+		// call returns in the next if clause.
+	}
+	if q0 < w.entryAddr {
+		w.entryAddr += q1 - q0
+		return
+	}
+
+	w.Addr("#%d", w.entryAddr)
+	text, err := w.ReadAll("data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("text after prompt=[%s]", text)
+	for {
+		i := bytes.IndexRune(text, '\n')
+		if i < 0 {
+			break
+		}
+
+		t := string(text[:i+1])
+		w.Addr("#%d,#%d", w.promptAddr, w.entryAddr + utf8.RuneCountInString(t))
+		msg := ""
+		if t != "\n" {
+			msg = w.privMsgString(*nick, t)
+		}
+		w.writeData([]byte(msg+prompt))
+
+		w.promptAddr += utf8.RuneCountInString(msg)
+		w.entryAddr = w.promptAddr + utf8.RuneCountInString(prompt)
+		text = text[i+1:]
+	}
+	w.Addr("#%d", w.promptAddr)
+}
+
+func (w *window) deleting(q0, q1 int) {
+	if q0 >= w.entryAddr {
+		return
+	}
+	if q1 >= w.entryAddr {
+		w.entryAddr = q0
+	} else {
+		w.entryAddr -= q1 - q0
+	}
+
+	if q0 >= w.promptAddr {
+		return
+	}
+	if q1 >= w.promptAddr {
+		w.promptAddr = q0
+	} else {
+		w.promptAddr -= q1 - q0
+	}
 }
 
 // user has information on a single user.
@@ -202,6 +380,11 @@ func handleWindowEvent(ev windowEvent) {
 		if len(fs) > 0 {
 			handleExecute(ev, fs[0], fs[1:])
 		}
+	} else if (ev.C1 == 'M' || ev.C1 == 'K') && ev.C2 == 'I' {
+		ev.typing(ev.Q0, ev.Q1)
+
+	} else if (ev.C1 == 'M' || ev.C1 == 'K') && ev.C2 == 'D' {
+		ev.deleting(ev.Q0, ev.Q1)
 	}
 }
 
@@ -357,40 +540,7 @@ func doPrivMsg(ch, who, text string) {
 		ch = who
 	}
 	w := getWindow(ch)
-
-	const actionPrefix = "\x01ACTION"
-	if strings.HasPrefix(text, actionPrefix) {
-		text = strings.TrimRight(text[len(actionPrefix):], "\x01")
-		w.WriteString(who + text)
-		w.lastMsgOrigin = ""
-		w.lastMsgTime = time.Now()
-		return
-	}
-
-	// Only print the user name if there is a
-	// new speaker or if two minutes has passed.
-	if who != w.lastMsgOrigin || time.Since(w.lastMsgTime).Minutes() > 2 {
-		origNick := ""
-		if u, ok := users[who]; ok {
-			// If the user hasn't change their nick in an hour
-			// then set this as the original nick name.
-			if time.Since(u.lastChange).Hours() > 1 {
-				u.origNick = u.nick
-			}
-			if u.nick != u.origNick {
-				origNick = " (" + u.origNick + ")"
-			}
-		}
-		w.WriteString(fmt.Sprintf("<%s>%s", who, origNick))
-	}
-	w.lastMsgOrigin = who
-	w.lastMsgTime = time.Now()
-
-	if strings.HasPrefix(text, *nick + ":") {
-		w.WriteString("*\t" + text)
-	} else {
-		w.WriteString("\t" + text)
-	}
+	w.WriteString(w.privMsgString(who, text))
 }
 
 func doNick(prev, cur string) {
