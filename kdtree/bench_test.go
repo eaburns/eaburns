5bdb09979a37d6235c00f6d128462528fa8a4c7f
Ethan Burns <burns.ethan@gmail.com>
2012-12-28 16:06:29 -0500

    kdtree: More benchmarks.
    Check the difference between building a K-D tree with 1000 nodes
    and one with 500 nodes.  Also, test lookups in a K-D tree built using
    New() and one created using repeated calls to Insert.

--- kdtree/bench_test.go
+++ kdtree/bench_test.go
@@ -30,6 +30,11 @@ func BenchmarkInsert1000(b *testing.B) {
 	insertSz(1000, b)
 }
 
+// BenchmarkInsert500  benchmarks 500 insertions into an empty tree.
+func BenchmarkInsert500(b *testing.B) {
+	insertSz(500, b)
+}
+
 // InsertSz benchmarks inserting sz nodes into an empty tree.
 func insertSz(sz int, b *testing.B) {
 	b.StopTimer()
@@ -53,6 +58,11 @@ func insertSz(sz int, b *testing.B) {
 // BenchmarkMake1000 benchmarks Make with 1000 nodes.
 func BenchmarkMake1000(b *testing.B) {
 	makeSz(1000, b)
+}
+
+// BenchmarkMake500 benchmarks Make with 500 nodes.
+func BenchmarkMake500(b *testing.B) {
+	makeSz(500, b)
 }
 
 // MakeSz benchmarks Make with a given number of nodes.
@@ -80,15 +90,17 @@ func makeSz(sz int, b *testing.B) {
 
 }
 
-// BenchmarkInRange1000 benchmarks the InRange function with
-// a tree containing 1000 nodes.
-func BenchmarkInRange1000(b *testing.B) {
-	inRangeSz(1000, b)
+func BenchmarkMakeInRange1000(b *testing.B) {
+	newInRangeSz(1000, b)
 }
 
-// inRangeSz benchmarks InRange function on a tree with the given
-// number of nodes.
-func inRangeSz(sz int, b *testing.B) {
+func BenchmarkMakeInRange500(b *testing.B) {
+	newInRangeSz(500, b)
+}
+
+// newInRangeSz benchmarks InRange function on a tree
+// created with New with the given number of nodes.
+func newInRangeSz(sz int, b *testing.B) {
 	b.StopTimer()
 	nodes := make([]T, sz)
 	nodeps := make([]*T, sz)
@@ -99,6 +111,47 @@ func inRangeSz(sz int, b *testing.B) {
 		nodeps[i] = &nodes[i]
 	}
 	tree := New(nodeps)
+
+	points := make([]Point, b.N)
+	for i := range points {
+		for j := range points[i] {
+			points[i][j] = rand.Float64()
+		}
+	}
+	rs := make([]float64, b.N)
+	for i := range rs {
+		rs[i] = rand.Float64()
+	}
+
+	pool := make([]*T, 0, sz)
+
+	b.StartTimer()
+	for i, pt := range points {
+		tree.InRange(pt, rs[i]*radiusMax, pool[:0])
+	}
+}
+
+func BenchmarkInsertInRange1000(b *testing.B) {
+	insertInRangeSz(1000, b)
+}
+
+func BenchmarkInsertInRange500(b *testing.B) {
+	insertInRangeSz(500, b)
+}
+
+// insertInRangeSz benchmarks InRange function on a tree
+// created with repeated calls to Insert with the given number
+// of nodes.
+func insertInRangeSz(sz int, b *testing.B) {
+	b.StopTimer()
+	var tree *T
+	for i := 0; i < sz; i++ {
+		n := new(T)
+		for j := range n.Point {
+			n.Point[j] = rand.Float64()
+		}
+		tree = tree.Insert(n)
+	}
 
 	points := make([]Point, b.N)
 	for i := range points {
